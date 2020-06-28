7047ad21982d613856d31f64bb8e20950cd95b34
AndreyOksenyuk <oksenyuk.andrey@gmail.com>
2020-06-28 21:04:23 +0300

    commit 2

--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@ window.addEventListener('scroll', function () {
    let title = document.querySelector('.title');
    let x = window.scrollY / 2000 ;
    
-   title.style.top = `${50 + (window.scrollY / 10)}%`
+   title.style.top = `${50 + (window.scrollY / 20)}%`
    sky.style.top = 0.5 * window.scrollY + 'px';
+   sky.style.transform = `scale(${1 + x/2})`;
    foliage.style.right =  0.2 * -window.scrollY + 'px'
    foliage.style.top =  0.4 * -window.scrollY + 'px'
    tree.style.left =  0.4 * -window.scrollY + 'px'
